89b3d332d75a13a8c6ce37944d76ec135793ead1
Luiz Francisco <141175753+Luizzz2505@users.noreply.github.com>
2024-12-29 21:06:19 -0300

    sql course

---
+++ sql_hashtag/SQLQuery5.sql
@@ -0,0 +1,30 @@
+-- Comando SELECT DISTINCT: Retorna os valores distintos de uma tabela 
+
+
+-- Retorne todas as linhas da tabela dimProduct
+
+SELECT 
+	*
+FROM
+	DimProduct
+
+-- Retorne os valores distintos da coluna ColorName da tabela dimProduct 
+
+SELECT
+	DISTINCT ColorName
+FROM
+	DimProduct
+
+-- Retorne todas as linhas da tabela dim»mployee
+
+SELECT 
+	*
+FROM
+	DimEmployee
+
+-- Retorne os valores distintos da coluna DepartmentName da tabela dimEmployee
+
+SELECT 
+	DISTINCT DepartmentName
+FROM
+	DimEmployee
